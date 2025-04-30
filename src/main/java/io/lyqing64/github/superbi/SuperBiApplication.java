8671bf86ed2f6b6c11f20ca08ef1fcad46a1528e
LyQing63 <1827304875@qq.com>
2025-04-30 13:46:51 +0800

    first commit

---
+++ src/main/java/io/lyqing64/github/superbi/SuperBiApplication.java
@@ -0,0 +1,13 @@
+package io.lyqing64.github.superbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperBiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lyqing64/github/superbi/SuperBiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lyqing64.github.superbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
