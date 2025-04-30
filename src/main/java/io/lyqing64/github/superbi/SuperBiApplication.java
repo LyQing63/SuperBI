81f6f680e019769d78118cb0ec9feb97aad9a25b
LyQing63 <1827304875@qq.com>
2025-04-30 17:17:21 +0800

    feat(env): 添加环境变量加载功能并初始化 MyBatis
    - 添加 EnvLoader 类以加载环境变量到系统
    - 在主类中引入环境变量加载逻辑
    - 添加 MyBatis 的 MapperScan 注解

--- src/main/java/io/lyqing64/github/superbi/SuperBiApplication.java
+++ src/main/java/io/lyqing64/github/superbi/SuperBiApplication.java
@@ -1,12 +1,16 @@
 package io.lyqing64.github.superbi;
 
+import io.lyqing64.github.superbi.utils.EnvLoader;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.lyqing64.github.superbi.mapper")
 public class SuperBiApplication {
 
     public static void main(String[] args) {
+        EnvLoader.loadEnvToSystem();
         SpringApplication.run(SuperBiApplication.class, args);
     }
 
